4aab6d23c5525c75ff72a4cb694578540ee4b51b
skeres <stephane.keresztes@gmail.com>
2020-07-29 12:48:44 +0200

    add java8 streams examples

--- src/main/java/com/mycompagny/execution/Execution001.java
+++ src/main/java/com/mycompagny/execution/Execution001.java
@@ -9,6 +9,7 @@ package com.mycompagny.execution;
 
         import java.util.Arrays;
         import java.util.List;
+        import java.util.stream.IntStream;
         import java.util.stream.Stream;
 
 public class Execution001
@@ -18,6 +19,11 @@ public class Execution001
         Execution001 execution = new Execution001();
         execution.doWork1();
         execution.doWork2();
+        execution.doWork3();
+        execution.doWork4();
+        execution.doWork5();
+        execution.doWork6();
+        execution.doWork7();
 
     }
 
@@ -66,4 +72,70 @@ public class Execution001
                 .findFirst()
                 .ifPresent(System.out::println);  // a1
     }
+
+
+    /**
+     * Besides regular object streams Java 8 ships with special kinds of streams for working with the primitive data types int, long and double.
+     * As you might have guessed it's IntStream, LongStream and DoubleStream.
+     *
+     * IntStreams can replace the regular for-loop utilizing IntStream.range():
+     *
+     *
+     * All those primitive streams work just like regular object streams with the following differences:
+     * Primitive streams use specialized lambda expressions, e.g. IntFunction instead of Function or IntPredicate instead of Predicate.
+     * And primitive streams support the additional terminal aggregate operations sum() and average():
+     *
+     *
+     */
+    void doWork3(){
+        IntStream.range(1, 4)
+                .forEach(System.out::println);
+
+        Arrays.stream(new int[] {1, 2, 3})
+                .map(n -> 2 * n + 1)
+                .average()
+                .ifPresent(System.out::println);  // 5.0
+
+        //Sometimes it's useful to transform a regular object stream to a primitive stream or vice versa.
+        //For that purpose object streams support the special mapping operations mapToInt(), mapToLong() and mapToDouble:
+        Stream.of("a1", "a2", "a3","b8")
+                .map(s -> s.substring(1))
+                //.mapToInt(Integer::parseInt) // "::" is a Double Colon Operator. It gives the same result as below
+                .mapToInt(value -> Integer.parseInt(value))
+                .max()
+                .ifPresent(System.out::println);  // 3
+
+        //Primitive streams can be transformed to object streams via mapToObj():
+        IntStream.range(1, 4)
+                .mapToObj(i -> "a" + i)
+                .forEach(System.out::println);
+
+        //Here's a combined example: the stream of doubles is first mapped to an int stream and than mapped to an object stream of strings:
+        Stream.of(1.0, 2.0, 3.0)
+                .mapToInt(Double::intValue)
+                .mapToObj(i -> "a" + i)
+                .forEach(System.out::println);
+
+    }
+
+    void doWork4(){
+
+    }
+
+    void doWork5(){
+
+    }
+
+    void doWork6(){
+
+
+    }
+
+    void doWork7(){
+
+
+    }
+
+
+
 }
